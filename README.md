a0231a593ee0028202205da87465a71119686170
Acshi Haggenmiller <acshikh@gmail.com>
2016-06-20 10:45:46 -0400

    initial commit of basic plugin structure

---
+++ README.md
@@ -0,0 +1,1 @@
+# discourse-osf-integration
---
+++ plugin.rb
@@ -0,0 +1,5 @@
+# name: osf-integration
+# about: An integration plug-in for the Open Science Framework
+# version: 0.0.1
+# authors: Center for Open Science
+enabled_site_setting :osf_integration_enabled
